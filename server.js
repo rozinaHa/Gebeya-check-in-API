c2e269e0d6e23625ebf27c552ba6ac83be1e6fb9
Mahder <mahderhaileslasse@gmail.com>
2018-04-03 00:11:53 -0900

    adding source code to server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-# This line is added by bereket ! 
+# This line is added by bereket !
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var Visitor = require('.src/models/visitorController');
+var bodyParser = require('body-parser');
+var routes = require('.src/routes/route');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/visitordb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.listen(port,function(){
+  console.log("check-in-api server started on port" + PORT);
+});
