75884ee2beb1ffc9e30d957d8a1e7b63d3cbce2d
Mahder <mahderhaileslasse@gmail.com>
2018-04-04 09:21:28 -0900

    deleting create_visitor functions

--- src/controllers/visitorController.js
+++ src/controllers/visitorController.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var visitor = mongoose.model('Visitors');
-var fs = require('fs');
+
+
 
 exports.list_all_visitors = function(req,res){
   visitor.find({},function(err,visitor){
@@ -10,21 +11,12 @@ exports.list_all_visitors = function(req,res){
   });
 };
 
-exports.create_a_visitor = function(req, res) {
-  var new_visitor = new visitor(req.body);
-  new_visitor.scanned_copy.data = fs.readFileSync(req.body.imgPath);
-  new_visitor.scanned_copy.data = 'image/jpg';
-  new_visitor.save(function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
+
 
 exports.read_a_visitor = function(req, res) {
-  visitor.findById(req.params.visitorId, function(err, task) {
+  visitor.findById(req.params.visitorId, function(err, visitor) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(visitor);
   });
 };
