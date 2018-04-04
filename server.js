8813c6747c3ffb5412f9253b1a02d736e5b51957
AAITbereket <bereketgebredingle@gmail.com>
2018-04-04 10:06:27 +0300

    configuration added. | server.js is editted accordingly

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+// ~ Load all module dependencies ~ //
 var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var Visitor = require('./src/models/visitorsModel');
 var bodyParser = require('body-parser');
 var routes = require('./src/routes/route');
 
+// ~ loading configuration ~ //
+var config = require('./config');
+
+// ~ Intialize app ~ // 
+var app = express();
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/visitordb');
+mongoose.connect(config.MONGODB_URL);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app);
 
-app.listen(port,function(){
-  console.log("check-in-api server started on port" + port);
+app.listen(config.HTTP_PORT,function(){
+  console.log("check-in-api server started on port" + config.HTTP_PORT);
 });
---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+// Load Module Dependencies
+
+
+module.exports = {
+    // HTTP PORT
+    HTTP_PORT: 9119,
+  
+    // MONGODB URL
+    MONGODB_URL: 'mongodb://localhost/visitordb'
+    
+};
+  
