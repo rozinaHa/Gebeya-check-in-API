f5a42f49a609a2d3d1d8d543afac4addf0457522
Mahder <mahderhaileslasse@gmail.com>
2018-04-03 21:37:42 -0900

    updating server.js file

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-# This line is added by bereket !
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
-var Visitor = require('.src/models/visitorController');
+var Visitor = require('./src/models/visitorsModel');
 var bodyParser = require('body-parser');
-var routes = require('.src/routes/route');
+var routes = require('./src/routes/route');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/visitordb');
@@ -16,5 +15,5 @@ app.use(bodyParser.json());
 routes(app);
 
 app.listen(port,function(){
-  console.log("check-in-api server started on port" + PORT);
+  console.log("check-in-api server started on port" + port);
 });
