1f0ed46644e0f00c3e563ae1cb76f49c85a7d348
Mahder <mahderhaileslasse@gmail.com>
2018-04-04 21:35:26 -0900

    correcting the structure

--- src/controllers/visitorController.js
+++ src/controllers/visitorController.js
@@ -11,7 +11,28 @@ exports.list_all_visitors = function(req,res){
   });
 };
 
+exports.create_a_visitor = function(req,res){
+  var new_visitor = new visitor(req.body);
+  console.log("uploads/" + req.file.filename);
+  new_visitor.scanned_copy = "uploads/" + req.file.filename;
+  new_visitor.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(visitor);
+  });
+};
 
+exports.update_a_visitor = function(req,res){
+  console.log(req.file.filename);
+  console.log(req.body);
+  req.body["scanned_copy"] = "uploads/" + req.file.filename;
+  console.log(req.body);
+  visitor.findOneAndUpdate({_id: req.params.visitorId}, req.body, {new: true}, function(err, visitor) {
+    if (err)
+      res.send(err);
+    res.json(visitor);
+  });
+};
 
 exports.read_a_visitor = function(req, res) {
   visitor.findById(req.params.visitorId, function(err, visitor) {
--- src/routes/route.js
+++ src/routes/route.js
@@ -18,37 +18,11 @@ module.exports = function(app){
   app.route('/visitors')
     .get(visitors.list_all_visitors);
 
-  app.post('/visitors',upload.single('images'),function(req,res){
-    var new_visitor = new visitor(req.body);
-    console.log("uploads/" + req.file.filename);
-    new_visitor.scanned_copy = "uploads/" + req.file.filename;
-    new_visitor.save(function(err, task) {
-      if (err)
-        res.send(err);
-      res.json(visitor);
-    });
-  });
+  app.post('/visitors',upload.single('images'),visitors.create_a_visitor);
 
     app.route('/visitors/:visitorId')
     .get(visitors.read_a_visitor)
     .delete(visitors.delete_a_visitor);
 
-    app.put('/visitors/:visitorId',upload.single('images'),function(req,res){
-      console.log(req.file.filename);
-      console.log(req.body);
-      req.body["scanned_copy"] = "uploads/" + req.file.filename;
-      console.log(req.body);
-      visitor.findOneAndUpdate({_id: req.params.visitorId}, req.body, {new: true}, function(err, visitor) {
-        if (err)
-          res.send(err);
-        res.json(visitor);
-      });
-    });
+    app.put('/visitors/:visitorId',upload.single('images'),visitors.update_a_visitor);
 };
-/*exports.update_a_visitor = function(req, res) {
-  visitor.findOneAndUpdate({_id: req.params.visitorId}, req.body, {new: true}, function(err, visitor) {
-    if (err)
-      res.send(err);
-    res.json(visitor);
-  });
-};*/
